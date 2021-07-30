b1864ce136bddd1ba13db6ccb8fac2d0c775ed64
aszt <386592336@qq.com>
2021-07-30 17:01:38 +0800

    config分布式配置中心

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// 需要运维人员发送刷新请求，拉取配置
+// curl -X POST "http://localhost:3355/actuator/refresh"
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+
+}
